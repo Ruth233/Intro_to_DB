ee76cf23d59a8b868b78a019e1d330266ed23c9e
Ruth233 <qweba369@gmail.com>
2025-08-17 16:27:03 +0000

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,10 +28,9 @@ CREATE TABLE IF NOT EXISTS customers (
     address TEXT
 );
 
--- 4. Orders Table (depends on Customers)
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT PRIMARY KEY,
-    customer_id INT,
+    customer_id INT NOT NULL,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
@@ -39,10 +38,9 @@ CREATE TABLE IF NOT EXISTS orders (
 -- 5. Order_Details Table (depends on Orders + Books)
 CREATE TABLE IF NOT EXISTS order_details (
     orderdetailid INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
-
